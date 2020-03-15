3ecf8e9912f81a1d0efae1de893131f81fa78442
anouq <anouq>
2020-03-15 11:40:19 +0100

    i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'trainbow-app-root',
@@ -12,4 +13,11 @@ export class AppComponent {
     public faInstagram = faInstagram;
     public faFacebook = faFacebook;
     public faEnvelope = faEnvelope;
+
+    constructor(
+        public translate: TranslateService
+    ) {
+        translate.addLangs(['en', 'nl']);
+        translate.setDefaultLang('nl');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { registerLocaleData } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import localeNL from '@angular/common/locales/nl';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AgendaComponent } from './agenda/agenda.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -16,7 +18,15 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        FontAwesomeModule
+        FontAwesomeModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: httpTranslateLoader,
+                deps: [HttpClient]
+            }
+        })
     ],
     providers: [
         { provide: LOCALE_ID, useValue: 'nl-NL' }
@@ -27,3 +37,7 @@ export class AppModule {
 }
 
 registerLocaleData(localeNL);
+
+export function httpTranslateLoader(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
