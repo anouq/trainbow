1ed04a1e4566790c5bc927200eac7fb53ed62696
anouq <anouq>
2020-03-15 10:08:38 +0100

    Set up grid + style content sections and navigation + added static text + use FontAwesome for icons + logo sizing + pride background radient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faClock, faEnvelope, faInfoCircle, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'trainbow-app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'trainbow';
+    public faTwitter = faTwitter;
+    public faInstagram = faInstagram;
+    public faFacebook = faFacebook;
+    public faEnvelope = faEnvelope;
+    public faMapMarkerAlt = faMapMarkerAlt;
+    public faClock = faClock;
+    public faInfoCircle = faInfoCircle;
 }
