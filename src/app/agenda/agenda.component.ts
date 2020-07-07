a3b954a76417abffe240efb2de4a6f3b32389ad4
anouq <anouq>
2020-07-07 14:29:24 +0200

    Only show agenda items that aren't in the past

--- src/app/agenda/agenda.component.ts
+++ src/app/agenda/agenda.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faClock, faInfoCircle, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import moment from 'moment-timezone';
 import { AGENDA_ITEMS } from '../../assets/content-items/agenda-items';
 import { AgendaItem } from '../model';
 
@@ -10,12 +11,15 @@ import { AgendaItem } from '../model';
 })
 export class AgendaComponent {
     public agendaItems: AgendaItem[];
+    public today = moment();
 
     public faMapMarkerAlt = faMapMarkerAlt;
     public faClock = faClock;
     public faInfoCircle = faInfoCircle;
 
     constructor() {
-        this.agendaItems = AGENDA_ITEMS;
+        this.agendaItems = AGENDA_ITEMS.filter((item) => {
+            return moment(item.date).isSameOrAfter(this.today);
+        });
     }
 }
